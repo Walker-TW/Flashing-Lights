5c9480249c68991d8e31da7aa39e84176a3f06b0
Walker-TW <thomas.w.walker@live.co.uk>
2021-08-13 16:03:13 +0100

    Added favicon and updated tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Walker-TW/Flashing-Lights"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Flashing Lights!
         </a>
       </header>
     </div>
